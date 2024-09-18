4281cb023e6f66a46e880f39acf01983e3decd47
pych1971 <pych1971@gmail.com>
2024-09-18 10:55:38 +0500

    2024-09-18-10-55

---
+++ 02/05/08.py
@@ -0,0 +1,10 @@
+x = int(input())
+a = x // 100
+b = x % 100 // 10
+c = x % 10
+print(a, b, c, sep='')
+print(a, c, b, sep='')
+print(b, a, c, sep='')
+print(b, c, a, sep='')
+print(c, a, b, sep='')
+print(c, b, a, sep='')
