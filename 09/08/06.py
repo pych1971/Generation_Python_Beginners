a2aa9b56624ea851843a2ada3d8e9b07222a815a
pych1971 <pych1971@gmail.com>
2024-10-01 09:46:06 +0500

    2024-10-01-09-46

--- 09/08/06.py
+++ 09/08/06.py
@@ -3,6 +3,11 @@ next_book = input()
 order_ok = 'YES'
 for i in range(n - 1):
     prev_book, next_book = next_book, input()
-    if next_book < prev_book:
+    if next_book[:next_book.find(' ')] < prev_book[:prev_book.find(' ')]:
+        order_ok = 'NO'
+    elif next_book[:next_book.find(' ')] == prev_book[:prev_book.find(' ')] and next_book[
+                                                                                next_book.find('«'):] < prev_book[
+                                                                                                        prev_book.find(
+                                                                                                            '«'):]:
         order_ok = 'NO'
 print(order_ok)
